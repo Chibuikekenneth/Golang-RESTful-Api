b3ce533867d5b00ddc75f892dae37ce4f8114654
Chibuike Kenneth <chibuikekenneth003@gmail.com>
2018-09-25 09:45:02 +0000

    var

--- main.go
+++ main.go
@@ -8,15 +8,18 @@ import (
 )
 
 type Person struct {
-	ID        string `json:"id,omitempty"`
-	Firstname string `json:"firstname,omitempty"`
-	Lastname  string `json:"lastname,omitempty"`
+	ID        string   `json:"id,omitempty"`
+	Firstname string   `json:"firstname,omitempty"`
+	Lastname  string   `json:"lastname,omitempty"`
+	Address   *Address `json:"address,omitempty"`
 }
 
 type Address struct {
 	City  string `json:"city,omitempty"`
 	State string `json:"state,omitempty"`
 }
+
+var people []Person
 
 func GetPersonEndpoint(w http.ResponseWriter, req *http.Request) {
 
